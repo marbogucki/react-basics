b5d424a517a3b2d98ae266a312ef58d32049b0ec
Marcin Bogucki <mar.bogucki@gmail.com>
2020-09-06 18:07:53 +0200

    adding loading and error handling in users component

--- src/views/Users/Users.js
+++ src/views/Users/Users.js
@@ -5,33 +5,59 @@ import { urlApi } from "../../config/api";
 import Heading from "../../components/Heading/Heading";
 import Section from "../../components/Section/Section";
 import UserItem from "./UserItem/UserItem";
+import Loading from "../../components/Loading/Loading";
+import ErrorList from "../../components/ErrorList/ErrorList";
 
 const Users = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
   const [usersList, setUsersList] = useState([]);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const { data } = await axios.get(`${urlApi}/users`);
-      setUsersList(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get(`${urlApi}/users`);
+        setUsersList(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1000);
+      }
     };
-
     fetchUsers();
 
     // PROMISE WITHOUT ASYNC/AWAIT
+    // setLoading(true);
     // axios
     //   .get(`${urlApi}/users`)
     //   .then((response) => response.data)
-    //   .then((users) => setUsersList(users));
+    //   .then((users) => {
+    //     setUsersList(users);
+    //     setLoading(false);
+    //   })
+    //   .catch((error) => {
+    //     setLoading(false);
+    //     setError(error.message);
+    //   });
   }, []);
 
   return (
     <Section className="users-page">
       <Heading title="My users" />
-      <ul className="users-item-list">
-        {usersList.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </ul>
+      {loading ? (
+        <Loading />
+      ) : (
+        (error && <ErrorList message={error} />) || (
+          <ul className="users-item-list">
+            {usersList.map((user) => (
+              <UserItem key={user.id} user={user} />
+            ))}
+          </ul>
+        )
+      )}
     </Section>
   );
 };
